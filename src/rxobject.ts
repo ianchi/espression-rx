ec888b46999063a18bb8a37ba49a998ccbed4452
Adrian Panella <ianchi74@outlook.com>
2018-08-19 12:51:35 -0500

    fix: use generics in RxObject for better typing

--- src/rxobject.ts
+++ src/rxobject.ts
@@ -5,7 +5,6 @@
  * https://opensource.org/licenses/MIT
  */
 
-import { keyedObject } from 'espression';
 import { BehaviorSubject } from 'rxjs';
 
 export const AS_OBSERVABLE = Symbol('asObservable'),
@@ -13,7 +12,7 @@ export const AS_OBSERVABLE = Symbol('asObservable'),
   IS_REACTIVE = Symbol('isReactive');
 
 /** Array mutating methods that must trigger emmit */
-const mutatingMethods = [
+const mutatingMethods: Array<string | number | symbol> = [
   'copyWithin',
   'fill',
   'pop',
@@ -27,50 +26,47 @@ const mutatingMethods = [
 
 // tslint:disable-next-line:naming-convention
 // tslint:disable-next-line:typedef
-export function RxObject(base: object) {
-  const propSubjects: { [prop: string]: BehaviorSubject<any> } = {};
-  const mainSubject = new BehaviorSubject<any>(base);
+export function RxObject<T extends object>(base: T): T {
+  const propSubjects: { [P in keyof T]?: BehaviorSubject<T[P]> } = {};
+  const mainSubject = new BehaviorSubject<T>(base);
 
-  const isArray = Array.isArray(base);
+  if (typeof base !== 'object') throw new Error('Base must be an object or array');
 
-  const proxy = new Proxy(base, {
-    get: (target: keyedObject, prop: any) => {
-      if (isArray) {
-        if (typeof target[prop] === 'function') {
-          if (mutatingMethods.indexOf(prop) >= 0) {
-            // tslint:disable-next-line:only-arrow-functions
-            return function(...args: any[]): any {
-              const ret = target[prop](...args);
-              mainSubject.next(target);
-              // emmit new values for all suscribed
-              for (const key in propSubjects) {
-                propSubjects[key].next(target[key]);
-              }
-              return ret;
-            };
+  const proxy = new Proxy<T>(base, {
+    get: (target: T, prop: keyof T | symbol) => {
+      const cb = target[<keyof T>prop];
+      if (Array.isArray(target) && typeof cb === 'function' && mutatingMethods.indexOf(prop) >= 0) {
+        // tslint:disable-next-line:only-arrow-functions
+        return function(...args: any[]): any {
+          // tslint:disable-next-line:ban-types
+          const ret = (cb as Function)(...args);
+          mainSubject.next(target);
+          // emmit new values for all suscribed
+          for (const key in propSubjects) {
+            propSubjects[key]!.next(target[key]);
           }
-        }
+          return ret;
+        };
       }
-
       if (prop === GET_OBSERVABLE) {
-        return (propName: any) =>
+        return (propName: keyof T) =>
           propName in propSubjects
             ? propSubjects[propName]
-            : (propSubjects[propName] = new BehaviorSubject<any>(
-                propName in target ? target[propName] : undefined
-              ));
+            : (propSubjects[propName] = new BehaviorSubject(target[propName]));
       } else if (prop === AS_OBSERVABLE) {
         return () => mainSubject.asObservable();
       } else if (prop === IS_REACTIVE) {
         return () => true;
       }
 
-      return target[prop];
+      return target[<keyof T>prop];
     },
 
-    set: (target: keyedObject, prop: string, value: any, _obj) => {
+    set: (target: T, prop: keyof T, value: any, _obj): boolean => {
       target[prop] = value;
-      if (propSubjects[<any>prop]) propSubjects[<any>prop].next(value);
+      const sub = propSubjects[prop];
+      if (sub) sub.next(value);
+
       mainSubject.next(target);
 
       return true;
