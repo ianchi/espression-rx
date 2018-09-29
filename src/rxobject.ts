28ae4e595ed00a5a6ca04f3e4da99aa2693fda62
Adrian Panella <ianchi74@outlook.com>
2018-09-29 10:59:46 -0500

    feat(RxObject): add custom handler support

--- src/rxobject.ts
+++ src/rxobject.ts
@@ -26,7 +26,7 @@ const mutatingMethods: Array<string | number | symbol> = [
 
 // tslint:disable-next-line:naming-convention
 // tslint:disable-next-line:typedef
-export function RxObject<T extends object>(base: T, deep = false): T {
+export function RxObject<T extends object>(base: T, deep = false, handler?: ProxyHandler<T>): T {
   const propSubjects: { [P in keyof T]?: BehaviorSubject<T[P]> } = {};
   const propSubscriptions: { [P in keyof T]?: Subscription } = {};
   const mainSubject = new BehaviorSubject<T>(base);
@@ -41,7 +41,17 @@ export function RxObject<T extends object>(base: T, deep = false): T {
   }
 
   const proxy = new Proxy<T>(base, {
-    get: (target: T, prop: keyof T | symbol) => {
+    get: (target: T, prop: keyof T | symbol, receiver: any) => {
+      if (prop === GET_OBSERVABLE) {
+        return (propName: keyof T) =>
+          propName in propSubjects
+            ? propSubjects[propName]
+            : (propSubjects[propName] = new BehaviorSubject(target[propName]));
+      } else if (prop === AS_OBSERVABLE) {
+        return () => mainSubject.asObservable();
+      } else if (prop === IS_REACTIVE) {
+        return () => true;
+      }
       const cb = target[<keyof T>prop];
       if (Array.isArray(target) && typeof cb === 'function' && mutatingMethods.indexOf(prop) >= 0) {
         // tslint:disable-next-line:only-arrow-functions
@@ -56,24 +66,15 @@ export function RxObject<T extends object>(base: T, deep = false): T {
           return ret;
         };
       }
-      if (prop === GET_OBSERVABLE) {
-        return (propName: keyof T) =>
-          propName in propSubjects
-            ? propSubjects[propName]
-            : (propSubjects[propName] = new BehaviorSubject(target[propName]));
-      } else if (prop === AS_OBSERVABLE) {
-        return () => mainSubject.asObservable();
-      } else if (prop === IS_REACTIVE) {
-        return () => true;
-      }
-
-      return target[<keyof T>prop];
+      return handler && handler.get ? handler.get(target, prop, receiver) : cb;
     },
 
-    set: (target: T, prop: keyof T, value: any, _obj): boolean => {
-      target[prop] = value;
+    set: (target: T, prop: keyof T, value: any, receiver: any): boolean => {
+      if (handler && handler.set) {
+        if (!handler.set(target, prop, value, receiver)) return false;
+      } else target[prop] = value;
       const sub = propSubjects[prop];
-      if (sub) sub.next(value);
+      if (sub) sub.next(target[prop]);
 
       mainSubject.next(target);
 
