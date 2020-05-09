c87992ee98de2a12d9a71536bbc68b85465e2ad5
Adrian Panella <ianchi74@outlook.com>
2020-05-08 19:43:21 -0500

    fix(combineMixed): remove object input
    It was non standard, complicated and not used

--- src/combineMixed.ts
+++ src/combineMixed.ts
@@ -5,7 +5,6 @@
  * https://opensource.org/licenses/MIT
  */
 
-import { keyedObject } from 'espression';
 import { combineLatest, isObservable, Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 
@@ -15,49 +14,20 @@ import { map } from 'rxjs/operators';
  * @param input Array of values or observables
  * @param asObservable If false and all the array is scalar, returns a scalar. Default: `true`
  */
-export function combineMixed(input: any[] | keyedObject, asObservable: true): Observable<any[]>;
-export function combineMixed(
-  input: any[] | keyedObject,
-  asObservable?: false
-): Observable<any[]> | any[] | object;
-export function combineMixed(
-  input: any[] | keyedObject,
-  asObservable: boolean = true
-): Observable<any[]> | any[] | object {
-  if (Array.isArray(input)) {
-    let pos = 0;
-    const isObs: Array<number | false> = [],
-      hasObs = input.reduce(
-        (acum, val, i) => (isObs[i] = isObservable(val) && pos++) !== false || acum,
-        false
-      ); // tslint:disable-line:no-conditional-assignment
-
-    if (!hasObs) return asObservable ? of(input) : input;
-
-    return combineLatest(input.filter((_val, index) => isObs[index] !== false)).pipe(
-      map(result => isObs.map((o, i) => (o === false ? input[i] : result[o])))
-    );
-  }
-
-  const isObs: keyedObject = {},
-    observables = [];
+export function combineMixed(input: any[], asObservable: true): Observable<any[]>;
+export function combineMixed(input: any[], asObservable: false): Observable<any[]> | any[];
+export function combineMixed(input: any[], asObservable = true): Observable<any[]> | any[] {
+  if (!Array.isArray(input)) throw new Error('Input must be an array');
   let pos = 0;
-  for (const prop in input) {
-    if (isObservable(input[prop])) {
-      isObs[prop] = pos++;
-      observables.push(input[prop]);
-    } else isObs[prop] = false;
-  }
+  const isObs: Array<number | false> = [],
+    hasObs = input.reduce(
+      (acum, val, i) => (isObs[i] = isObservable(val) && pos++) !== false || acum,
+      false
+    ); // tslint:disable-line:no-conditional-assignment
 
-  if (!observables.length) return asObservable ? of(input) : input;
+  if (!hasObs) return asObservable ? of(input) : input;
 
-  return combineLatest(observables).pipe(
-    map(res => {
-      const result: keyedObject = {};
-      for (const prop in isObs) {
-        result[prop] = isObs[prop] === false ? input[prop] : res[isObs[prop]];
-      }
-      return result;
-    })
+  return combineLatest(input.filter((_val, index) => isObs[index] !== false)).pipe(
+    map(result => isObs.map((o, i) => (o === false ? input[i] : result[o])))
   );
 }
