d7ea6c93a736c20b36fb46c5c5e06263a7dbbf08
Adrian Panella <ianchi74@outlook.com>
2020-05-23 19:07:02 -0500

    chore: add debug of RxObject

--- spec/support/debug.ts
+++ spec/support/debug.ts
@@ -6,12 +6,13 @@
  */
 
 import { ESnextParser } from 'espression';
-import { isObservable } from 'rxjs';
 import { TestScheduler } from 'rxjs/testing';
 
 import { reactiveEvalFactory } from '../../src/reactive';
+import { RxObject, GET_OBSERVABLE } from '../../src/main';
+import assert from 'assert';
 
-const rx = reactiveEvalFactory();
+const rxEval = reactiveEvalFactory();
 const parser = new ESnextParser(true, undefined, undefined, false);
 
 // eslint-disable-next-line @typescript-eslint/no-require-imports
@@ -23,6 +24,25 @@ export function* makeIterator() {
 }
 
 function testObservable() {
+  const values = {
+    0: 0,
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9,
+    a: 'a',
+    b: 'b',
+    c: 'c',
+    o: { 1: 'o1', 2: 'o2', 3: 'o3' },
+    p: { 1: 'p1', 2: 'p2', 3: 'p3' },
+    q: { 1: 'q1', 2: 'q2', 3: 'q3' },
+    u: undefined,
+  };
   const testScheduler = new TestScheduler((actual, expected) => {
     const diff = variableDiff(expected, actual);
 
@@ -31,18 +51,23 @@ function testObservable() {
     } else console.log('Test OK');
   });
 
-  testScheduler.run(helpers => {
-    const { expectObservable, cold } = helpers;
-    const expr = 'a + o';
-    const source = '1-2-3|';
-    const result = '2-3-4|';
-    const expectedValues = { 1: 1, 2: 2, 3: 3, 4: 4 };
-    const context = { x: 1, o: cold(source, expectedValues) };
-    const e1 = rx.evaluate(parser.parse(expr), context);
+  testScheduler.run(({ expectObservable, cold, flush }) => {
+    const expr = 'rx.x = o';
+    const proxy1 = '--9-8-7|';
+    const result = '5-9-8-7-';
+    const subscr = '^-----------!';
 
-    if (isObservable(e1)) {
-      expectObservable(e1).toBe(result, expectedValues);
-    }
+    const rx = RxObject({ x: 5 });
+
+    const context = { a: 1, b: 5, rx, o: cold(proxy1, values) };
+    const e1 = rxEval.evaluate(parser.parse(expr), context);
+
+    expectObservable(e1).toBe(proxy1, values);
+    expectObservable(rx[GET_OBSERVABLE]('x'), subscr).toBe(result, values);
+
+    flush();
+
+    assert.equal(rx.x, 7);
   });
 }
 
